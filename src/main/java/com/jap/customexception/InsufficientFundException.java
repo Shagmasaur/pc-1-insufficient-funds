927448b1e2913fea5e6ea9061774e67e8ed52085
abhishek.lashley <abhishek.lashley@gmail.com>
2022-08-24 15:21:40 +0530

    InsufficientFundException DOne

--- src/main/java/com/jap/customexception/InsufficientFundException.java
+++ src/main/java/com/jap/customexception/InsufficientFundException.java
@@ -1,5 +1,10 @@
 package com.jap.customexception;
 
-public class InsufficientFundException extends Exception {
+public class InsufficientFundException extends Exception
+{
+    public InsufficientFundException(String message)
+    {
+        super(message);
+    }
 
 }
