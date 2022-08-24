208942b6fd7f5b80d073cc7fe464720a9863fabe
abhishek.lashley <abhishek.lashley@gmail.com>
2022-08-24 15:25:04 +0530

    Test cases passed. coding done

--- src/main/java/com/jap/customexception/AccountMain.java
+++ src/main/java/com/jap/customexception/AccountMain.java
@@ -2,6 +2,12 @@ package com.jap.customexception;
 
 public class AccountMain
 {
+    public static void main(String[] args) throws InsufficientFundException {
 
+        Account account =new Account(10000);
+
+        System.out.println("Account balance = " +account.getAccountBalance());
+        System.out.println("New Balance = " +account.withdraw(5000));
+    }
 
 }
