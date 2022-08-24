cca00ddf8236f862bca8178928da69eea0331b0c
abhishek.lashley <abhishek.lashley@gmail.com>
2022-08-24 14:59:40 +0530

    getter and setter added

--- src/main/java/com/jap/customexception/Account.java
+++ src/main/java/com/jap/customexception/Account.java
@@ -4,16 +4,21 @@ public class Account {
 
     private int accountBalance;
 
-    public Account(int accountBalance) {
+    public Account(int accountBalance)
+    {
         this.accountBalance = accountBalance;
     }
 
     //create getter and setter
 
 
+    public int getAccountBalance() {
+        return accountBalance;
+    }
 
-
-
+    public void setAccountBalance(int accountBalance) {
+        this.accountBalance = accountBalance;
+    }
 
     /**
      * This method returns the new balance deducting the withdraw amount from the balance
